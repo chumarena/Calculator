26cda2e00784944d77e148d8d27475a0a90c2a4e
Рената <r.ajusheva@gmail.com>
2025-11-23 01:09:42 +0300

    Add src files

---
+++ src/ln.cpp
@@ -0,0 +1,22 @@
+#include "plugin.h" 
+#include <cmath>
+#include <stdexcept>
+
+class LnPlugin : public IPlugin {
+public:
+    std::string getName() const override { return "ln"; }
+    OpType getType() const override { return OpType::UNARY; }
+    
+
+    double calculate(double a, double b = 0.0) override {
+        if (a <= 0.0) {
+            throw std::runtime_error("Error ln of a non-positive number");
+        }
+        return std::log(a); 
+    }
+};
+
+
+extern "C" __declspec(dllexport) IPlugin* createPlugin() {
+    return new LnPlugin();
+}
